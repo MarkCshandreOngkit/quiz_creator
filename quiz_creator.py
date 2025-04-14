cdfda8f709b0e0644bf406f0820bf6167f6f6aad
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-04-15 00:28:48 +0800

    custiomizable file name

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,3 +1,5 @@
+import os
+
 #initialization
 iteration = 1
 ask_exit = ""
@@ -17,34 +19,37 @@ def exit():
         exit()
 
 #create file
-with open("quiz.txt", "a") as file:
+with open("temp.txt", "a") as file:
 
     #loop
     while True:
         #ask for a question and write in file
-        question = input("Question:\n>")
+        question = input("Question:\n> ")
         file.write(f"Question {iteration}: " + question + "\n")
 
         #ask for 4 answers and write in file
-        answer_a = input("Answer A:\n>")
+        answer_a = input("Answer A:\n> ")
         file.write("A: " + answer_a + "\n")
 
-        answer_b = input("Answer B:\n>")
+        answer_b = input("Answer B:\n> ")
         file.write("B: " + answer_b + "\n")
 
-        answer_c = input("Answer C:\n>")
+        answer_c = input("Answer C:\n> ")
         file.write("C: " + answer_c + "\n")
 
-        answer_d = input("Answer D:\n>")
+        answer_d = input("Answer D:\n> ")
         file.write("D: " + answer_d + "\n")
 
         #ask for correct answer and write in file
-        correct_answer = input("Correct Answer:\n>")
+        correct_answer = input("Correct Answer:\n> ")
         file.write("Correct Answer: " + correct_answer + "\n")
         
         exit()
 
         if exit_decision == True:
             break
+        
+    file_name = input("File Name:\n> ") + ".txt"
+os.rename("temp.txt", file_name)
             
              
