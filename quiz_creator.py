871e2d6c10105f6ae8519b1018d00ee987a4e6bf
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-04-14 23:13:37 +0800

    loop and ask questions

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,5 +1,12 @@
 #loop
-#ask for a question
-#ask for 4 answers
-#ask for correct answer
-#write in a file
+while True:
+    #ask for a question
+    question = input("Question:\n>")
+    #ask for 4 answers
+    answer_a = input("Answer A:\n>")
+    answer_b = input("Answer B:\n>")
+    answer_c = input("Answer C:\n>")
+    answer_d = input("Answer D:\n>")
+    #ask for correct answer
+    correct_answer = input("Correct Answer:\n>")
+    #write in a file
