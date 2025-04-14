287b7bf6c9e97f9f946177e72d6b740f9643b0a2
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-04-14 23:35:06 +0800

    create file and write inputs in file

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,12 +1,26 @@
-#loop
-while True:
-    #ask for a question
-    question = input("Question:\n>")
-    #ask for 4 answers
-    answer_a = input("Answer A:\n>")
-    answer_b = input("Answer B:\n>")
-    answer_c = input("Answer C:\n>")
-    answer_d = input("Answer D:\n>")
-    #ask for correct answer
-    correct_answer = input("Correct Answer:\n>")
-    #write in a file
+#create file
+with open("quiz.txt", "a") as file:
+    #loop
+    while True:
+        #ask for a question and write in file
+        question = input("Question:\n>")
+        file.write(question + "\n")
+
+        #ask for 4 answers and write in file
+        answer_a = input("Answer A:\n>")
+        file.write(answer_a + "\n")
+
+        answer_b = input("Answer B:\n>")
+        file.write(answer_b + "\n")
+
+        answer_c = input("Answer C:\n>")
+        file.write(answer_c + "\n")
+
+        answer_d = input("Answer D:\n>")
+        file.write(answer_d + "\n")
+
+        #ask for correct answer and write in file
+        correct_answer = input("Correct Answer:\n>")
+        file.write(correct_answer + "\n")
+        break
+
